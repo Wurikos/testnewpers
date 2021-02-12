3ce3fc740ccea10887ab64de91b51c060505d51a
Sulin Alexander <50768680+Wurikos@users.noreply.github.com>
2021-02-12 13:45:31 +0300

    Changes

---
+++ src/pages/index.js
@@ -0,0 +1,103 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { Section, Override, StackItem, Stack } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text
+					as="h4"
+					font="--base"
+					color="--grey"
+					letter-spacing="1px"
+					text-transform="uppercase"
+					margin="6px 0"
+				>
+					About me
+				</Text>
+				<Text as="h2" font="--headline2" margin="0 0 12px 0">
+					Hey I’m David
+				</Text>
+				<Text font="--base">
+					My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+				</Text>
+			</Box>
+		</Section>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						margin-right="-20%"
+						padding-bottom="120%"
+						background="url(https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80) 50% 15%/cover"
+						lg-margin-right="0"
+						lg-padding-bottom="80%"
+					/>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						padding="98px 64px"
+						margin-left="-20%"
+						margin-top="36px"
+						background="--color-red"
+						color="--light"
+						mix-blend-mode="lighten"
+						lg-margin-left="0"
+						lg-margin-top="0"
+						flex-direction="column"
+					/>
+					<Text
+						as="h4"
+						margin="0"
+						font="--base"
+						letter-spacing="1px"
+						text-transform="uppercase"
+					>
+						About me
+					</Text>
+					<Text as="h1" margin="0 0 16px 0" font="--headline1" lg-font="--headline2">
+						Hey I’m David
+					</Text>
+					<Text as="p" margin="16px 0" font="--base" max-width="400px">
+						My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6011283b5a9875001e4c824a",
+				"6011283b5a9875001e4c824c"
+			]
+		},
+		"6011283b5a9875001e4c824a": {
+			"id": "6011283b5a9875001e4c824a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6011283b5a9875001e4c824c": {
+			"id": "6011283b5a9875001e4c824c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
